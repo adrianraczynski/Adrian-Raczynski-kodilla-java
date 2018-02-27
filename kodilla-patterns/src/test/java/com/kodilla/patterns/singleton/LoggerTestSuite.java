bc8dd295ef77b4e761ace0b3aa330cdbbd1c7bbb
“adrianraczynski <adrianraczynski7@gmail.com>
2018-02-27 15:58:25 +0100

    zadanie 11.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.*;
+
+public class LoggerTestSuite {
+    private static Logger logger;
+
+    @BeforeClass
+    public static void beforeTest () {
+       System.out.println("This is the beginning of tests.");
+       logger = new Logger();
+       logger.getInstance().log("01234");
+    }
+
+    @AfterClass
+    public static void afterTests () {
+        System.out.println("All tests are finished.");
+    }
+
+    @Test
+    public void getLastLog() {
+        //Given
+        //When
+        String testLog = logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals("01234", testLog);
+    }
+}
