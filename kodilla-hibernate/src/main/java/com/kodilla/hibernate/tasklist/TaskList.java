40c35cd87b65272b3c9f72d404f1eb2cc19e765e
“adrianraczynski <adrianraczynski7@gmail.com>
2018-03-17 14:26:40 +0100

    Zad 13.3 - problem z teorią

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,8 +1,12 @@
 package com.kodilla.hibernate.tasklist;
 
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
@@ -10,13 +14,14 @@ public class TaskList {
 
     private int id;
     private String listName;
-    private int description;
+    private String description;
+    private List <Task> tasks = new ArrayList <>();
 
     public TaskList () {
 
     }
 
-    public TaskList(String listName, int description) {
+    public TaskList(String listName, String description) {
         this.listName = listName;
         this.description = description;
     }
@@ -35,7 +40,7 @@ public class TaskList {
     }
 
     @Column(name="DESCRIPTION")
-    public int getDescription() {
+    public String getDescription() {
         return description;
     }
 
@@ -47,7 +52,21 @@ public class TaskList {
         this.listName = listName;
     }
 
-    public void setDescription(int description) {
+    public void setDescription(String description) {
         this.description = description;
+    }
+
+    @OneToMany (
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,5 +1,7 @@
 package com.kodilla.hibernate.tasklist.dao;
 
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
 import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -16,13 +19,14 @@ import java.util.List;
 public class TaskListDaoTestSuite {
     @Autowired
     private TaskListDao taskListDao;
-    private static final String HALLO = "Test: I don't know what i'm doing ;/";
+    private static final String HALLO = "Test: I don't know what i'm doing.";
+    private static final String LISTNAME = "List of tasks from exercise 13.3";
 
 
     @Test
     public void testFindByListName() {
         //Given
-        TaskList taskList = new TaskList(HALLO, 5);
+        TaskList taskList = new TaskList("My new tasks", HALLO);
         taskListDao.save(taskList);
         String listName = taskList.getListName();
 
@@ -34,6 +38,38 @@ public class TaskListDaoTestSuite {
 
         //CleanUp
         int id = readTaskList.get(0).getId();
+        taskListDao.delete(id);
+    }
+
+    @Test
+    public void testTaskListDaoSaveWithTasks() {
+        //Given
+        //Given
+        Task task = new Task("Test: Learn Hibernate", 14);
+        Task task2 = new Task("Test: Write some entities", 3);
+
+        TaskFinancialDetails tfd = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+
+        task.setTaskFinancialDetails(tfd);
+        task2.setTaskFinancialDetails(tfd2);
+
+        TaskList taskList = new TaskList(LISTNAME, "ToDo tasks");
+
+        taskList.getTasks().add(task);
+        taskList.getTasks().add(task2);
+
+        task.setTaskList(taskList);
+        task2.setTaskList(taskList);
+
+        //When
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+
+        //CleanUp
         taskListDao.delete(id);
     }
 }
