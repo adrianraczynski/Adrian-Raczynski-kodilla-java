493da2f798e1b2aadd2034098e620b9f8ebb6fa7
adrianraczynski <adrianraczynski7@gmail.com>
2018-04-28 16:10:15 +0200

    Zad 20.4 - v.2

--- kodilla-pattern2/src/main/java/com/kodilla/pattern2/observer/homework/StudentAgata.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.pattern2.observer.homework;
-
-public class StudentAgata extends HomeworkQueue {
-
-    public StudentAgata(){
-        super ("Zad 1.1");
-    }
-}
--- kodilla-pattern2/src/main/java/com/kodilla/pattern2/observer/homework/StudentPawel.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.pattern2.observer.homework;
-
-public class StudentPawel extends HomeworkQueue {
-
-    public StudentPawel(){
-        super ("Zad 1.1");
-    }
-}
