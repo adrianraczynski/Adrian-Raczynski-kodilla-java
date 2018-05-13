013e6cc3f04309a21162decd39c1638c2146ff6a
adrianraczynski <adrianraczynski7@gmail.com>
2018-05-13 14:58:09 +0200

    zad 22.3

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertEquals;
 
 public class StoredProcTestSuite {
 
+    //testy dla modułu 22.3
     @Test
     public void testUpdateVipLevels() throws SQLException {
         //Given
@@ -28,5 +29,25 @@ public class StoredProcTestSuite {
             howMany = resultSet.getInt("HOW_MANY");
         }
         assertEquals(0, howMany);
+    }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER=-1";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+        //When
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement.execute(sqlProcedureCall);
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER=0";
+        ResultSet resultSet = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (resultSet.next()) {
+            howMany = resultSet.getInt("HOW_MANY");
+        }
+        assertEquals(2, howMany);
     }
 }
